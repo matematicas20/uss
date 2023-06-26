d259469300586c371c7fda0b74b9b179e84aaf68
matematicas20 <matematicas20@gmail.com>
2023-06-26 17:39:04 -0500

    Update precios.js

--- precios.js
+++ precios.js
@@ -1,5 +1,5 @@
  const datosJSON = [
-          { Condominio: 'Condominio 1', 'Tipo de Lote': 'Lote vista interna', Precio: 'S/ 150000' },
+          { Condominio: 'Condominio 1', 'Tipo de Lote': 'Lote vista interna', Precio: 'S/ 300000' },
           { Condominio: 'Condominio 2', 'Tipo de Lote': 'Lote vista al parque', Precio: 'S/ 150000' },
           { Condominio: 'Condominio 2', 'Tipo de Lote': 'Lote vista interna', Precio: 'S/ 150000' },
           { Condominio: 'Condominio 2', 'Tipo de Lote': 'Lote comercial', Precio: 'S/ 150000' },
